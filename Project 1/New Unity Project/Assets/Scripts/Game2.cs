9567fe6ec357b6a92b80e17b7de45f898cf8ce6b
Brandon Portelli <a100817@mcast.org.mt>
2017-10-11 13:36:00 +0200

    First PRoject

---
+++ Project 1/New Unity Project/Assets/Scripts/Game2.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game2 : MonoBehaviour {
+
+    int num1 = 0;
+    int max = 100;
+    int min = 0;
+    int answer = 50;
+
+    void NextGuess()
+    {
+        print("Enter a number between - " + min + " and " + max);
+    }
+
+    void StartGame()
+    {
+        print("Up adds 1. Down subtracts 1. Right adds 10. Left subtracts 10.");
+        print("---------------");
+        NextGuess();
+    }
+
+    void Start () {
+        StartGame();
+	}
+	
+	
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            print("+1");
+            NextGuess();
+        }
+        else
+
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            print("-1");
+            NextGuess();
+        }
+        else
+        
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            print("+10");
+            NextGuess();
+        }
+        else
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            print("-10");
+            NextGuess();
+        }
+    }
+}
---
+++ Project 1/New Unity Project/Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberWizard : MonoBehaviour {
+
+    int max = 1000;
+    int min = 1;
+    int guess = 0;
+
+    void NextGuess()
+    {
+        print("Please enter a number from " + min + " and " + max);
+        print("Up for Bigger, Down for smaller, = for equal.");
+    }
+
+    void StartGame()
+    {
+        print("---------------");
+        Debug.Log("Please enter a number from " + min + "and " + max + guess);
+        print("Think of a number.");
+        NextGuess();
+    }
+
+    void Start () {
+        StartGame();
+	}
+
+    void Update() {
+
+        
+    }  
+}
